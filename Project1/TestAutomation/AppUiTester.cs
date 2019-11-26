90075aa13621288047820c08284e3c21d84c19ab
Marcin Bobel <marcin.bobel7@gmail.com>
2019-11-26 17:19:49 +0100

    Added automat for UI tests

---
+++ Project1/TestAutomation/AppUiTester.cs
@@ -0,0 +1,85 @@
+﻿using StorageHelper;
+using System;
+using System.Linq;
+using TestStack.White;
+using TestStack.White.Factory;
+using TestStack.White.UIItems;
+using TestStack.White.UIItems.ListBoxItems;
+using TestStack.White.UIItems.WindowItems;
+
+namespace TestAutomation
+{
+    internal static class AppUiTester
+    {
+        public static void TestServerApp(Computer computer, bool createXml, int rowId)
+        {
+            Application application = Application.Launch("Project2.exe");
+            Window window = application.GetWindow("Integracja Systemów - Marcin Bobel", InitializeOption.NoCache);
+            Button loadDataFromCsvButton = window.Get<Button>("LoadDataFromCsvButton");
+            loadDataFromCsvButton.Click();
+
+            ListView priceList = window.Get<ListView>("PriceList");
+
+            if (priceList == null || priceList.Items.Count < rowId)
+                throw new ArgumentException("RowId grather than avaiable rows");
+
+
+            ListViewRow row = priceList.Rows[rowId];
+            var properties = computer.GetType().GetProperties().ToList();
+
+            for (int i = 0; i < properties.Count; i++)
+            {
+                var value = properties[i].GetValue(computer);
+                if (value == null)
+                    continue;
+
+                row.Cells[i].Click();
+                row.Cells[i].Enter("");
+                row.Cells[i].Enter(value.ToString());
+                row.Cells[i].Click();
+            };
+
+            if (createXml)
+            {
+                Button saveToXmlButton = window.Get<Button>("ExportToXmlButton");
+                saveToXmlButton.Click();
+            }
+
+            Button saveToDbButton = window.Get<Button>("ExportToDbButton");
+            saveToDbButton.Click();
+        }
+
+        public static void TestClientApp(string producer, string displayType)
+        {
+            Application application = Application.Launch("Project2.exe");
+            Window window = application.GetWindow("Integracja Systemów - Marcin Bobel", InitializeOption.NoCache);
+            Button openClientAppButton = window.Get<Button>("GoToClientAppButton");
+            openClientAppButton.Click();
+
+            Window clientWindow = application.GetWindow("ClientApp", InitializeOption.NoCache);
+            if (producer != null)
+            {
+                ComboBox comboBox =  clientWindow.Get<ComboBox>("ProducersComboBox");
+                if(comboBox.Items.Any(p => p.Text == producer)){
+                    comboBox.Select(producer);
+                }
+
+            }
+
+            Button getProducerLaptopsCountButton = clientWindow.Get<Button>("GetProducerLaptopsCountButton");
+            getProducerLaptopsCountButton.Click();
+
+            if (displayType != null)
+            {
+                ComboBox comboBox = clientWindow.Get<ComboBox>("DisplayType");
+                if(comboBox.Items.Any(p => p.Text == displayType))
+                {
+                    comboBox.Select(displayType);
+                }
+            }
+
+            Button getLabptopsByDisplayTypeButton = clientWindow.Get<Button>("GetLabptopsByDisplayTypeButton");
+            getLabptopsByDisplayTypeButton.Click();
+        }
+    }
+}
---
+++ Project1/TestAutomation/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using StorageHelper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TestStack.White.Factory;
+using White = TestStack.White;
+
+namespace TestAutomation
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Computer computer;
+        public MainWindow()
+        {
+            InitializeComponent();
+            computer = new Computer();
+            this.DataContext = computer;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            Computer testCaseComputer = new Computer()
+            {
+                Producer = ProducerCheckbox.IsChecked.Value ? computer.Producer : null,
+                ScreenSize = ScreenSizeCheckbox.IsChecked.Value ? computer.ScreenSize : null,
+                ScreenResolution = ResolutionCheckbox.IsChecked.Value ? computer.ScreenResolution : null,
+                DisplayType = DisplayTypeCheckbox.IsChecked.Value ? computer.DisplayType : null,
+                Processor = ProcessorCheckbox.IsChecked.Value ? computer.Processor : null,
+                PhysicalCores = PhysicalCoresCheckbox.IsChecked.Value ? computer.PhysicalCores : null,
+                CpuClockSpeed = CpuClockSpeedCheckbox.IsChecked.Value ? computer.CpuClockSpeed : null,
+                Ram = RamCheckbox.IsChecked.Value ? computer.Ram : null,
+                DiscType = DiscTypeCheckbox.IsChecked.Value ? computer.DiscType : null,
+                DiscSize = DiscSizeCheckbox.IsChecked.Value ? computer.DiscSize : null,
+                Gpu = GpuCheckbox.IsChecked.Value ? computer.Gpu : null,
+                GpuMemory = GpuCheckbox.IsChecked.Value ? computer.GpuMemory : null,
+                OperatingSystem = OSCheckbox.IsChecked.Value ? computer.OperatingSystem : null,
+                OpticalDrive = OpticalDriveCheckbox.IsChecked.Value ? computer.OpticalDrive : null
+            };
+
+            if (int.TryParse(RowId.Text, out int parsedId))
+            {
+                AppUiTester.TestServerApp(testCaseComputer, SaveToXmlCheckbox.IsChecked.Value, parsedId);
+            }
+            else
+            {
+                MessageBox.Show("Cannot parse rowId");
+            }
+
+            AppUiTester.TestClientApp(computer.Producer, computer.DisplayType);
+        }
+    }
+}
